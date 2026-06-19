8a058736f598ed063120f3ea54ab86c61df78fc4
Jyothi Grandhi <jyothigrandhi.26@gmail.com>
2026-06-20 00:44:49 +0530

    Add root route

--- backend/server.js
+++ backend/server.js
@@ -27,3 +27,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+app.get("/", (req, res) => {
+  res.send("SocialGram Backend Running");
+});
